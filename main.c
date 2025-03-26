be43e5830ffaaedb9267034bdd77258083f54f11
Jovialngandu <jovialngandu2@gmail.com>
2025-03-26 02:06:52 +0100

    [ADD|UPDATE] Preparation de la rule deduction

--- main.c
+++ main.c
@@ -24,6 +24,20 @@ int main()
 
         if (selectedProperties) {
 
+           for (int i = 0; i < selectedPropertiesSize; i++) {
+                printf("Caractéristique ID (via Property): %d -> Propriété sélectionnée: %s (ID: %d)\n",
+               selectedProperties[i].characteristic_id, selectedProperties[i].libelle, selectedProperties[i].id);
+             }
+             printf("size %d\n",selectedPropertiesSize);
+        
+        // if (allPropertyRules) {
+        //     printf("PropertyRules regroupées par Property:\n");
+        //     for (int i = 0; i < allPropertyRules->size; i++) {
+        //         printf("\nProperty ID: %d, Libelle: %s\n", properties[i].id, properties[i].libelle);
+        //         afficherPropertyRuleArray(allPropertyRules->data[i]);
+        //     }
+        //     freePropertyRuleArrayList(allPropertyRules);
+
             Fact *matchingFacts = findFactsWithAllProperties(datas, selectedProperties, selectedPropertiesSize, &matchingFactsSize);
 
             if (matchingFacts) {
@@ -34,7 +48,12 @@ int main()
                 }
                 free(matchingFacts);
             } else {
-                printf("Aucun fait trouvé\n");
+                PropertyRuleArrayList* allPropertyRules = generatePropertyRulesByPropertyFromJSON(datas, properties, numProperties);
+                PropertyRuleArrayList* groupedByRule = groupPropertyRulesByRuleId(allPropertyRules);
+                MatchesList *rematch= matchPropertyRuleArray(selectedProperties,selectedPropertiesSize,groupedByRule);
+                printMatchesList(rematch);
+                // afficherPropertyRuleArrayList(groupedByRule);
+                freePropertyRuleArrayList(groupedByRule);
             }
 
             free(selectedProperties);
